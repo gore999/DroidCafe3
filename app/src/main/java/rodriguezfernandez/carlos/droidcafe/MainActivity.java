711581996c8eb3f8bacaadc1f6fa1f07ba2e1206
Carlos <carloscoucho@gmail.com>
2019-02-26 00:40:52 +0100

    Leasson 4.3 Add items to the options menu: Task 3: Añadir acciones a los iconos. Al hacer click en order, se abre una activity diferente.

--- app/src/main/java/rodriguezfernandez/carlos/droidcafe/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/droidcafe/MainActivity.java
@@ -47,8 +47,21 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_order) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.action_order:
+                Intent intent=new Intent(MainActivity.this,OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE,mOrderMMessaje);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
